3d4960cc9744679005c6e51cc755cc06ef0b94e8
sun123zxy <304244450@qq.com>
2019-05-03 20:49:24 +0800

    working update
    rt,trying to add settings on popup.html&popup.js

---
+++ js/background.js
@@ -0,0 +1,1 @@
+console.log("successful loaded background.js")
---
+++ js/popup.js
@@ -0,0 +1,12 @@
+console.log("successful loaded popup.js")
+//var bg=chrome.extension.getBackgroundPage();
+/*
+document.getElementById("confirm").onclick=function(){
+    var shell=document.getElementById("settings");
+    console.log(shell);
+    var sets=shell.childNodes;
+    for(var i=0;i<sets.length;i++){
+      var id=sets[i].getAttribute("id");
+      localStorage[id]=sets[i].value;
+    }
+}*/
