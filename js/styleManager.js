77d7d8ae2c58deb40dd70b57adad5e975bcbcabe
sun123zxy <304244450@qq.com>
2019-05-12 23:44:09 +0800

    Working Update:Messages
    rt

--- js/styleManager.js
+++ js/styleManager.js
@@ -1,10 +1,18 @@
 console.log("successful loaded styleManager.js");
 var root=document.querySelector(':root');
 
-//TODO：background-contentJs通信
-/*
-chrome.storage.local.get(['bgImage'],function(result){
-    root.setAttribute("style","--bgImage: " + result.key);
-});*/
-//root.setAttribute("style","--bgImage: "+chrome.storage.local['bgImage']);//TODO
-//chrome.storage.local.get(['bgImage'],function(result){console.log(result)});
+function getLocalStorage(callBack){//找bg要localStorage
+    chrome.runtime.sendMessage({type:'getLocalStorage'}, function(response) {
+        callBack(response);
+    });
+}
+
+getLocalStorage(function(storage){
+    console.log('successfully get the storage:');
+    console.log(storage);
+    for(var idx in storage){
+        root.style.setProperty('--'+idx,storage[idx]);
+    }
+    console.log("final style:");
+    console.log(root.style);
+});
