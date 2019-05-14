7d7ae8cd7a02a4a1db35d7ef55000adb3105cebe
sun123zxy <304244450@qq.com>
2019-05-14 22:44:47 +0800

    unstable update

--- js/styleManager.js
+++ js/styleManager.js
@@ -6,13 +6,20 @@ function getLocalStorage(callBack){//找bg要localStorage
         callBack(response);
     });
 }
+function refreshStyle(){
+    console.log('refreshing styles...');
+    getLocalStorage(function(storage){
+        console.log('successfully get the storage:');
+        console.log(storage);
+        for(var idx in storage){
+            if(idx.substring(0,6)=='style_'){//判断是否为要用的缓存
+                var name='--'+idx.substring(6);
+                root.style.setProperty(name,storage[idx]);
+            }
+        }
+        console.log("final style:");
+        console.log(root.style);
+    });
+}
 
-getLocalStorage(function(storage){
-    console.log('successfully get the storage:');
-    console.log(storage);
-    for(var idx in storage){
-        root.style.setProperty('--'+idx,storage[idx]);
-    }
-    console.log("final style:");
-    console.log(root.style);
-});
+refreshStyle();
