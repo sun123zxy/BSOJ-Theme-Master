5881c650836b85b44edd9028444e634c5c6757a8
sun123zxy <304244450@qq.com>
2019-05-17 22:39:08 +0800

    working update
    fixes

--- js/popup.js
+++ js/popup.js
@@ -3,6 +3,7 @@ var bgPage = chrome.extension.getBackgroundPage();
 var shell=document.getElementById("settings");
 var settings=shell;
 var confirm=document.getElementById("confirm");
+var reset=document.getElementById("reset");
 var ip=document.getElementById("import");
 var ep=document.getElementById("export");
 var jsonInput=document.getElementById("jsonInput");
@@ -23,15 +24,10 @@ function refresh(){//读取缓存并填入settings中的INPUT
   findINPUT(settings,function(x){
     var id=x.getAttribute('id');
     x.value=bgPage.localStorage[id];
+    if(x.value=='undefined') x.value='';//防出现undefined后confirm导致css默认值失效
   });
 }
-confirm.onclick=function(){
-  findINPUT(settings,function(x){//将settings中的所有INPUT值存入缓存
-    var id=x.getAttribute('id');
-    bgPage.localStorage[id]=x.value;
-  });
-  console.log("successfully saved!");
-};
+//-----Onclicks-----
 ip.onclick=function(){/*导入JSON*/
   var json = JSON.parse(jsonInput.value);
   jsonInput.value="";
@@ -45,5 +41,17 @@ ep.onclick=function(){/*导出JSON*/
   jsonInput.value=JSON.stringify(bgPage.localStorage);
   console.log("successfully exported!");
 };
+confirm.onclick=function(){
+  findINPUT(settings,function(x){//将settings中的所有INPUT值存入缓存
+    var id=x.getAttribute('id');
+    bgPage.localStorage[id]=x.value;
+  });
+  console.log("successfully saved!");
+};
+reset.onclick=function(){
+  bgPage.localStorage.clear();
+  refresh();
+  console.log("successfully reset!");
+}
 //-----Begin-----
 refresh();
